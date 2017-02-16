cdc586d970ba248573a0f309cbff51f9b649f0f0
ahmadaram <ahmadaram34@gmail.com>
2017-02-16 17:28:29 -0500

    Added Buttons and CHanges to xml stuff

--- app/src/main/java/com/cs321_005/hangman/MainActivity.java
+++ app/src/main/java/com/cs321_005/hangman/MainActivity.java
@@ -1,18 +1,42 @@
 package com.cs321_005.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button howToPlay;
+    Button newGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Testing",Toast.LENGTH_SHORT).show();
-        Log.d("GitTesting","Test Git");
-        Log.d("GitTesting","AHmads Test Message");
+
+        newGame = (Button) findViewById(R.id.newgame);
+        howToPlay = (Button) findViewById(R.id.Help);
+        Toast.makeText(getApplicationContext(), "Testing", Toast.LENGTH_SHORT).show();
+        Log.d("GitTesting", "Test Git");
+        Log.d("GitTesting", "Ahmads Test Message");
+        Log.d("TestMessages", "Test2");
+        newGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent newGame = new Intent(getBaseContext(),NewGame.class);//intents allow you to transfer data over transfer variables over to new class
+                startActivity(newGame);
+            }
+        });
+
+        howToPlay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent newGame = new Intent(getBaseContext(),NewGame.class);//intents allow you to transfer data over transfer variables over to new class
+                startActivity(newGame);
+            }
+        });
+
     }
 }
+
+
---
+++ app/src/main/java/com/cs321_005/hangman/NewGame.java
@@ -0,0 +1,21 @@
+package com.cs321_005.hangman;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NewGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+
+
+
+    }
+
+}
