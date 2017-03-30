c929b55bf1b746b717f61c1e8b6e1b19cb7798ad
ahmadaram <ahmadaram34@gmail.com>
2017-03-30 15:26:17 -0400

    Removed content_activity.xml

--- app/src/main/java/com/cs321_005/hangman/Difficulty.java
+++ app/src/main/java/com/cs321_005/hangman/Difficulty.java
@@ -2,14 +2,12 @@ package com.cs321_005.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class HowToPlay extends AppCompatActivity {
+public class Difficulty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_how_to_play);
-        String hTPText = "Hello";
+        setContentView(R.layout.activity_difficulty);
     }
 }
